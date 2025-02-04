1a28dde5b1ca2fc8096e342a3000857d65fcab23
PioCang <piovincecang@gmail.com>
2025-02-04 18:40:32 +0800

    model: Create model and migrations

--- flexisource/flexitrade/models.py
+++ flexisource/flexitrade/models.py
@@ -1,3 +1,75 @@
+"""The models used by the Flexitrade app.
+As echoed in the README file, these premises are esoteric only.
+
+1. Users have unlimited purchasing power.
+2. Stocks have an unlimited number for shares for trading.
+3. The value of the stocks remain constant.
+4. A user can unilaterally buy or sell a stock anytime i.e., no counterparty.
+5. Fractional shares cannot be bought or sold.
+6. The market is always open for trading.
+"""
+
+from decimal import Decimal
+
+from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    """An abstract base class model that provides self-updating `created` and
+    `modified` fields.
+    """
+
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta info"""
+
+        abstract = True
+
+
+class Stock(TimeStampedModel):
+    """The model that represents a Stock.
+
+    Will use the auto-incremented Django-provided `id` as the primary key
+    instead of manually defining it, so there's a backup surrogate key if need
+    be. The ticker symbol is limited to 5 characters, as in the Nasdaq.
+    """
+
+    ticker_symbol = models.CharField(max_length=5, unique=True)
+    name = models.CharField(max_length=256)
+    price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        validators=[MinValueValidator(0.01)],
+        default=Decimal(0.0),
+    )
+
+
+class Trade(TimeStampedModel):
+    """The model that represents a trade by a User on a given Stock.
+
+    This should answer the question: Who did what to which stock, in what
+    quantity, and when?
+    """
+
+    class TradeAction(models.TextChoices):
+        BUY = ("b", "User buys.")
+        SELL = ("s", "User sells.")
+
+    actor = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="trades",
+        on_delete=models.PROTECT,
+    )
+    stock = models.ForeignKey(
+        Stock, related_name="traded_in", on_delete=models.PROTECT
+    )
+    action = models.CharField(
+        max_length=1, choices=TradeAction, default=TradeAction.BUY
+    )
+    quantity = models.IntegerField(
+        validators=[MinValueValidator(1)], default=1
+    )
